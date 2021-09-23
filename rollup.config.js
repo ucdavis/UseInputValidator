24cec1a5c9e4529ad687d755914a13c8a3ed7ec3
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-09-23 14:07:24 -0700

    package using rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from "@rollup/plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
+import cleanup from "rollup-plugin-cleanup";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    { file: pkg.main, format: "cjs", dir: "dist" },
+    { file: pkg.module, format: "esm", dir: "dist" },
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+    }),
+    del({ targets: ["dist/*"] }),
+    typescript(),
+    cleanup({ comments: "none" }),
+  ],
+  external: Object.keys(pkg.peerDependencies || {}),
+};
--- src/lib/TextInput.tsx
+++ src/lib/TextInput.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./TextInput.css";
 
 interface Props {
   type: string;
